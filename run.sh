cfa07b734044ce9f9ecc9901e0b62b7108989f45
abhithakre <68857990+abhithakre@users.noreply.github.com>
2020-08-03 17:26:15 +0530

    Update run.sh

--- run.sh
+++ run.sh
@@ -2,10 +2,10 @@
 
 set -x
 
-PROJECT_KEY="${PROJECT_KEY:-ConsoleApplication1}"
-PROJECT_NAME="${PROJECT_NAME:-ConsoleApplication1}"
+PROJECT_KEY="${PROJECT_KEY:-abhithakre_DotNetAPi}"
+PROJECT_NAME="${PROJECT_NAME:-DotNetAPi}"
 PROJECT_VERSION="${PROJECT_VERSION:-1.0}"
-SONAR_HOST="${HOST:-http://localhost:9000}"
+SONAR_HOST="${HOST:-https://sonarcloud.io/}"
 SONAR_LOGIN_KEY="${LOGIN_KEY:-admin}"
 
 mono /opt/sonar-scanner-msbuild/SonarScanner.MSBuild.exe begin /d:sonar.host.url=$SONAR_HOST /d:sonar.login=$SONAR_LOGIN_KEY /k:$PROJECT_KEY /n:"$PROJECT_NAME" /v:$PROJECT_VERSION
